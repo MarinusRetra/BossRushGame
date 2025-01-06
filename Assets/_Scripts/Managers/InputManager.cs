e3bfe4848c9473605d60e03f4c47761cc35efe42
Niek Melet <niek+git@meletstudios.com>
2025-01-06 11:01:53 +0100

    Set up InputManager for full dev

--- Assets/_Scripts/Managers/InputManager.cs
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -1,10 +1,12 @@
 using BossRush.Utility;
+using System;
 using UnityEngine;
 
 namespace BossRush.Managers
 {
     public class InputManager : Singleton<InputManager>
     {
-        
+        public PlayerInputSystem InputSystem;
+        public PlayerInputSystem.PlayerActions PlayerActions;
     }
 }
